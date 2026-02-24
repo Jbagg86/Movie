282d8ebfbfd286b09f4db67422bc0d38a14087b1
Johnathan Baggett <jbagg1986@gmail.com>
2026-02-24 12:15:42 -0500

    Add project files.

---
+++ Movie/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Movie.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
